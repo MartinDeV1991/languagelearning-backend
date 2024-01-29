6fc7759f31b5b49ded3d93fbf7b5532190bb4cd8
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2024-01-29 11:42:38 +0100

    Translate word + context sentence with deepl

--- src/main/java/com/devteam/languagelearning/service/WordService.java
+++ src/main/java/com/devteam/languagelearning/service/WordService.java
@@ -1,8 +1,12 @@
 package com.devteam.languagelearning.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import com.deepl.api.DeepLException;
+import com.deepl.api.TextResult;
+import com.deepl.api.Translator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,13 @@ public class WordService {
 	@Autowired
 	private WordRepository wordRepository;
 
+	private final Translator translator;
+
+	@Autowired
+	public WordService(Translator translator) {
+		this.translator = translator;
+	}
+
 	public List<Word> getAllWords() {
 		return wordRepository.findAll();
 	}
@@ -22,5 +33,32 @@ public class WordService {
 	public Optional<Word> findById(long id) {
 		return this.wordRepository.findById(id);
 	}
-	
+
+	public Word addWord(Word word) {
+			try {
+				// translate both word and sentence with deepl, save to the word object
+                TextResult translatedWord = translator.translateText(word.getWord(), word.getSourceLanguage(), word.getTranslatedTo());
+				TextResult translatedSentence = translator.translateText(word.getContextSentence(), word.getSourceLanguage(), word.getTranslatedTo());
+				word.setTranslation(translatedWord.getText());
+				word.setTranslatedContextSentence(translatedSentence.getText());
+
+				// set source language from API response if not already provided
+				if (word.getSourceLanguage() == null) {
+					word.setSourceLanguage(translatedSentence.getDetectedSourceLanguage());
+				}
+
+				// save and return new word object
+				return this.wordRepository.save(word);
+
+			} catch (DeepLException | InterruptedException e) {
+				System.out.println("Exception: " + e);
+				throw new RuntimeException(e);
+			}
+	}
+
+	public Word deleteWord(long id) throws NoSuchElementException {
+		Word word = wordRepository.findById(id).orElseThrow();
+		wordRepository.deleteById(id);
+		return word;
+	}
 }
