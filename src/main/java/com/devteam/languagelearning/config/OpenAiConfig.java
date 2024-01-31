c81a050cc420844374a54f9868586659f8775196
MartinDeV1991 <131389779+MartinDeV1991@users.noreply.github.com>
2024-01-31 22:35:55 +0100

    Update OpenAiConfig.java

--- src/main/java/com/devteam/languagelearning/config/OpenAiConfig.java
+++ src/main/java/com/devteam/languagelearning/config/OpenAiConfig.java
@@ -6,14 +6,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenAiConfig {
 
-    @Value("${OPENAI_API_KEY}")
-    private static String openAiKey;
+    // @Value("${OPENAI_API_KEY}")
+    // private static String openAiKey;
 
-//    private static final String openAiKey = System.getenv("OPENAI_API_KEY");
+   private static String openAiKey = System.getenv("OPENAI_API_KEY");
 //    private static final String openAiKey = "test";
     
     public static String getOpenAiKey() {
     	System.out.println("key1234: " + openAiKey);
+        if (openAiKey == null || openAiKey.isEmpty()) {
+    		openAiKey = "key";
+    	}
         return openAiKey;
     }
 }
