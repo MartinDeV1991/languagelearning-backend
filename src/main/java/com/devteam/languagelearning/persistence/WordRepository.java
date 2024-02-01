82e592bbc5eeffe2a44c8502d13a787ff202372b
Martin <martin_de_v@hotmail.com>
2024-02-01 18:26:57 +0100

    added endpoint for words by user

--- src/main/java/com/devteam/languagelearning/persistence/WordRepository.java
+++ src/main/java/com/devteam/languagelearning/persistence/WordRepository.java
@@ -1,6 +1,8 @@
 package com.devteam.languagelearning.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devteam.languagelearning.model.User;
 import com.devteam.languagelearning.model.Word;
 import org.springframework.data.repository.query.Param;
 
@@ -8,4 +10,5 @@ import java.util.List;
 
 public interface WordRepository extends JpaRepository<Word, Long>{
     List<Word> findByRootWordId(@Param("rootWordId") long id);
+    List<Word> findByUser(User user);
 }
