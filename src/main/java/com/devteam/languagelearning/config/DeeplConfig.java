4c1dc312bc5f7be6fdabdf09b44939e01dbce629
Martin <martin_de_v@hotmail.com>
2024-02-05 16:20:39 +0100

    conflicts resolution

--- src/main/java/com/devteam/languagelearning/config/DeeplConfig.java
+++ src/main/java/com/devteam/languagelearning/config/DeeplConfig.java
@@ -8,15 +8,17 @@ import org.springframework.beans.factory.annotation.Value;
 @Configuration
 public class DeeplConfig {
 
-    // @Value("${DEEPL_API_KEY}")
-    // private String deeplApiKey;
+     @Value("${DEEPL_API_KEY}")
+     private String deeplApiKey1;
 
-   private String deeplApiKey = System.getenv("DEEPL_API_KEY");;
+   private String deeplApiKey;
    // private String deeplApiKey = "test";
 
     @Bean
     public Translator translator() {
+        deeplApiKey = System.getenv("DEEPL_API_KEY");
     	System.out.println("key1: " + deeplApiKey);
+    	System.out.println("key2: " + deeplApiKey1);
     	if (deeplApiKey == null || deeplApiKey.isEmpty()) {
     		deeplApiKey = "key";
     	}
